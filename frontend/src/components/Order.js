0cc4a74e1220d2cb0cb6e0d4ca0283b270397951
hughesmatthew1 <116681449+hughesmatthew1@users.noreply.github.com>
2024-03-26 22:32:10 -0400

    Update Order.js
    Added description of what this page will evetually do

--- frontend/src/components/Order.js
+++ frontend/src/components/Order.js
@@ -5,7 +5,9 @@ function Order() {
         <section>
             <div>
                 <h1 class="page-head">Place an order</h1>
-                <p>lost on how to do so</p>
+                <p>Query if user is logged in w jwt</p>
+                <p>If not logged in, say log in to add items to cart, provide link to account page</p>
+                <p>If logged in, display cart</p>
             </div>
         </section>
         // Order page html
